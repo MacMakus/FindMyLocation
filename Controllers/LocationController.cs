e433075384968e1a5c0265a1d8fcf3ad244d3695
MacMakus <119934279+MacMakus@users.noreply.github.com>
2024-12-26 14:54:23 +0700

    Add files via upload

---
+++ Controllers/LocationController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FindMyLocation.Controllers
+{
+    public class LocationController : Controller
+    {
+        public IActionResult Home()
+        {
+            return View();
+        }
+
+        public IActionResult GetMyLocation()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult RedirectToGoogleMaps(double latitude, double longitude)
+        {
+            string googleMapsUrl = $"https://www.google.com/maps?q={latitude},{longitude}";
+            return Redirect(googleMapsUrl);
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FindMyLocation.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
